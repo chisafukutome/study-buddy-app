1c9d4ae8134534c7c059f11398b27aedaf8c55b7
chisa-f <kiki4537fer2cat@gmail.com>
2022-09-04 02:35:08 -0400

    Add sign up for study session functionality. Fix small things.

--- server.py
+++ server.py
@@ -16,7 +16,6 @@ db = SQLAlchemy(app)
 
 # --Study Session
 class Study_Session(db.Model):
-    # TODO: add who created
     id = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String(250), nullable=False)
     date = db.Column(db.Date, nullable=False)
@@ -24,6 +23,7 @@ class Study_Session(db.Model):
     end_time = db.Column(db.String, nullable=False)
     location = db.Column(db.String(300), nullable=False)
     description = db.Column(db.String(500), nullable=True)
+    signed_up = db.Column(db.Boolean, default=False)
 
 # --User
 class User(db.Model):
@@ -36,8 +36,11 @@ db.create_all()
 
 
 
-def read_study_session():
-    return db.session.query(Study_Session).all()
+def read_not_signed_up_session():
+    return Study_Session.query.filter_by(signed_up=False).all()
+
+def read_signed_up_session():
+    return Study_Session.query.filter_by(signed_up=True).all()
 
 
 @app.route("/")
@@ -62,8 +65,9 @@ def home():
         return redirect(url_for('main'))
     print("rendering home")
     username = session['username']
-    study_sesison_list = read_study_session()
-    return render_template("index.html", study_session_list=study_sesison_list, username=username)
+    study_sesison_list = read_not_signed_up_session()
+    signed_up_session_list = read_signed_up_session()
+    return render_template("index.html", study_session_list=study_sesison_list, username=username, signed_up_session_list=signed_up_session_list)
 
 @app.route("/create_study_session")
 def create_study_session():
@@ -116,6 +120,20 @@ def login():
     if session['username']:
         return 'Your username is '+session['username']
     return render_template("login.html")
+
+@app.route("/sign_up")
+def sign_up():
+    session_id = request.args.get("session_id")
+
+    selected_session = Study_Session.query.get(session_id)
+    selected_session.signed_up = True
+    db.session.commit()
+
+    return redirect(url_for('home'))
+
+@app.route("/signin")
+def signin():
+    return render_template("signin.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
